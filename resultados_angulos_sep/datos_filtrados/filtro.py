b1c30b894152a7d969309770f1900d5d3cce62ff
Pablo Buitron <pablyandi@hotmail.com>
2023-06-12 22:38:13 -0500

    Datos filtrados a 500 m2 de área lluvia de 10 minutos

--- resultados_angulos_sep/datos_filtrados/filtro.py
+++ resultados_angulos_sep/datos_filtrados/filtro.py
@@ -3,14 +3,14 @@ import matplotlib.pyplot as plt
 import random
 
 # Generar valores aleatorios para n y m
-n = 60
+n = 70
 m = 90
 
 # Cargar los datos desde el archivo con separación por espacios
-data = pd.read_csv("60sec90deg_000060.csv", delimiter="\s+")
+data = pd.read_csv("70-90deg_000600.csv", delimiter="\s+")
 
 # Filtrar los datos con "x" y "y" menores o iguales a 1000
-filtered_data = data[(data["x"].abs() <= 1000) & (data["y"].abs() <= 1000)]
+filtered_data = data[(data["x"].abs() <= 500) & (data["y"].abs() <= 500)]
 
 # Realizar un plot de dispersión entre las columnas "x" y "y"
 plt.scatter(filtered_data["x"], filtered_data["y"])
@@ -19,11 +19,11 @@ plt.ylabel("y")
 plt.title("Distancia al detector, ángulos entre 70° a 90°")
 
 # Obtener el nombre del archivo sin la extensión
-nombre_archivo = f"{n}sec{m}deg"
+nombre_archivo = f"{n}-{m}deg_000600"
 
 # Guardar el gráfico en una imagen PNG con el nuevo nombre
 #plt.savefig(nombre_archivo + ".png")
-plt.savefig("filter-" + nombre_archivo + ".png")
+plt.savefig("filter-" + nombre_archivo + "_500m.png")
 
 # Hacer la preselección de datos según CorsikaId y prm_theta
 preseleccion = filtered_data[filtered_data["prm_theta"] >= 0]
@@ -32,7 +32,7 @@ preseleccion = filtered_data[filtered_data["prm_theta"] >= 0]
 tablas_por_corsika_id = preseleccion.groupby("CorsikaId")
 
 # Guardar las tablas resultantes en un archivo de texto
-with open("filter_head_60sec90deg.txt", "w") as file:
+with open("filter_head_70-90deg_500m.txt", "w") as file:
     for corsika_id, tabla in tablas_por_corsika_id:
         file.write(f"Tabla para CorsikaId {corsika_id}:\n")
         file.write(tabla.to_string())
