70bb7d2c7f557c36ade9082dee43697ef9dd0f5b
Pablo Buitron <pablyandi@hotmail.com>
2023-06-14 01:07:27 -0500

    Cambios en script de ángulo polar, más simulaciones, ahora con 30 minutos y más intentos de plots de restricción polar

--- restriccion_polar.sh
+++ restriccion_polar.sh
@@ -1,17 +1,27 @@
 #!/bin/bash
 
-# Obtener la ubicación del directorio desde la bandera -d o --directorio
-while [[ "$#" -gt 0 ]]; do
-    case $1 in
-        -d|--directorio) directorio="$2"; shift ;;
-        *) echo "Opción inválida: $1"; exit 1 ;;
+# Obtener las opciones de las banderas usando getopts
+while getopts "d:m:n:" opcion; do
+    case $opcion in
+        d)
+            directorio=$OPTARG
+            ;;
+        m)
+            numero_linea_14=$OPTARG
+            ;;
+        n)
+            numero_linea_15=$OPTARG
+            ;;
+        \?)
+            echo "Opción inválida: -$OPTARG" >&2
+            exit 1
+            ;;
     esac
-    shift
 done
 
 # Verificar si se proporcionó la ubicación del directorio
 if [ -z "$directorio" ]; then
-    echo "Debes proporcionar la ubicación del directorio con la opción -d/--directorio."
+    echo "Debes proporcionar la ubicación del directorio con la opción -d."
     exit 1
 fi
 
@@ -25,9 +35,9 @@ fi
 for archivo in "$directorio"/*.run; do
     # Verificar si el archivo es legible
     if [[ -r "$archivo" ]]; then
-        # Modificar las líneas 14 y 15 del archivo
-        sed -i '14s/.*/-90/' "$archivo"
-        sed -i '15s/.*/90/' "$archivo"
+        # Modificar las líneas 14 y 15 del archivo con los números de reemplazo proporcionados
+        sed -i "14s/.*/$numero_linea_14/" "$archivo"
+        sed -i "15s/.*/$numero_linea_15/" "$archivo"
         echo "Archivo modificado: $archivo"
     fi
 done
--- resultados_angulos_sep/datos_filtrados/restriccion_polar/filtro.py
+++ resultados_angulos_sep/datos_filtrados/restriccion_polar/filtro.py
@@ -3,11 +3,11 @@ import matplotlib.pyplot as plt
 import random
 
 # Generar valores aleatorios para n y m
-n = 0
-m = 70
+n = 70
+m = 90
 
 # Cargar los datos desde el archivo con separación por espacios
-data = pd.read_csv("0-70deg-rpol-der_000100.csv", delimiter="\s+")
+data = pd.read_csv("70-90deg-rpol-der_001800.csv", delimiter="\s+")
 
 # Filtrar los datos con "x" y "y" menores o iguales a 1000
 filtered_data = data[(data["x"].abs() <= 500) & (data["y"].abs() <= 500)]
@@ -16,10 +16,10 @@ filtered_data = data[(data["x"].abs() <= 500) & (data["y"].abs() <= 500)]
 plt.scatter(filtered_data["x"], filtered_data["y"])
 plt.xlabel("x")
 plt.ylabel("y")
-plt.title("Distancia al detector, ángulos entre 0° a 70°")
+plt.title("Distancia al detector, ángulos entre 70° a 90°")
 
 # Obtener el nombre del archivo sin la extensión
-nombre_archivo = f"{n}-{m}deg_000100"
+nombre_archivo = f"{n}-{m}deg_0001800"
 
 # Guardar el gráfico en una imagen PNG con el nuevo nombre
 #plt.savefig(nombre_archivo + ".png")
@@ -32,7 +32,7 @@ preseleccion = filtered_data[filtered_data["prm_theta"] >= 0]
 tablas_por_corsika_id = preseleccion.groupby("CorsikaId")
 
 # Guardar las tablas resultantes1 en un archivo de texto
-with open("filter_head_0-70deg_003600_500m_rest_pol_der.txt", "w") as file:
+with open("filter_head_70-90deg_001800_500m_rest_pol_der.txt", "w") as file:
     for corsika_id, tabla in tablas_por_corsika_id:
         file.write(f"Tabla para CorsikaId {corsika_id}:\n")
         file.write(tabla.to_string())
